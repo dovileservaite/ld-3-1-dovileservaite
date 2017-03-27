0a3a209c8668847fe1c1c2fbc79803753f0b9de7
User1 <User1@DESKTOP-DS1E0BG>
2017-03-27 11:35:46 +0300

    Add project files.

---
+++ 1 pavyzdys/1 pavyzdys.cpp
@@ -0,0 +1,15 @@
+// 1 pavyzdys.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>   //01_cpp.cpp
+using namespace std;
+int main()
+{
+	int a = 10;
+	cout <<"kintamojo a reiksme "<< a << endl;
+	cout <<"kintamojo a adresas "<< &a << endl;
+
+	system("pause");
+	return 0;
+}
+
